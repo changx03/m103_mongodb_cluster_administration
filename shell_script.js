8a26619ff38c6d4ed8b79790cbf3e549fc6fea16
Luke Chang <luke.x.chang@gmail.com>
2019-04-13 01:59:37 +1200

    added script for basic security

--- shell_script.js
+++ shell_script.js
@@ -9,8 +9,38 @@ db.auth('m103-admin', 'm103-pass')
 
 // Logging basics
 db.getLogComponents()
-db.adminCommand({ 'getLog': 'global' })
+db.adminCommand({ getLog: 'global' })
 db.setLogLevel(0, 'index')
 
 // set global verbosity
 db.setLogLevel(1)
+
+/**
+ * profiler
+ */
+db.getProfilingLevel()
+db.setProfilingLevel(1)
+
+// show collections
+// system.profile
+
+// example: set Level 1 `slow ms` to 0, and then insert a doc to a new collection.
+db.setProfilingLevel(1, { slowms: 0 })
+db.col.insert({ msg: 'Hello world' })
+db.system.profile.find().pretty()
+db.col.find()
+db.system.profile.find().pretty()
+// planSummary: 'COLLSCAN'
+
+/**
+ * Basic security
+ */
+// use admin
+db.createUser({
+  user: 'root',
+  pwd: 'root',
+  roles: ['root']
+})
+
+// auth user and then run
+db.stats()
