310b39bbd15417ac637233cc2735b456a137c5e7
Luke Chang <luke.x.chang@gmail.com>
2019-04-13 00:13:12 +1200

    added script for logging

--- shell_script.js
+++ shell_script.js
@@ -4,5 +4,13 @@ db.createUser({
   roles: [{ role: 'root', db: 'admin' }]
 })
 
-use admin
+// use admin
 db.auth('m103-admin', 'm103-pass')
+
+// Logging basics
+db.getLogComponents()
+db.adminCommand({ 'getLog': 'global' })
+db.setLogLevel(0, 'index')
+
+// set global verbosity
+db.setLogLevel(1)
